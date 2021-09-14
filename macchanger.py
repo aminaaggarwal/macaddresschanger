1ea14ade772a8c53b45801d61f5a75d67e6c2e9f
Amina Aggarwal <amina.aggarwal@workday.com>
2021-09-14 22:24:12 +1200

    first commit

---
+++ macchanger.py
@@ -0,0 +1,5 @@
+import subprocess
+subprocess.call("ifconfig", shell="True")
+subprocess.call("ifconfig en0 down", shell="True")
+subprocess.call("ifconfig en0 hw ether 00:11:22:33:44:55", shell="true")
+subprocess.call("ifconfig en0 up", shell="True")
